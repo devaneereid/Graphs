920b83f2985af13826132023c3f001b528e2320e
Devanee <devaneereid@gmail.com>
2020-09-09 16:01:43 -0600

    ancestor tests passing now

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -4,7 +4,7 @@ class Queue():
     def enqueue(self, value):
         self.queue.append(value)
     def dequeue(self):
-        if self.size > 0:
+        if self.size() > 0:
             return self.queue.pop(0)
         else:
             return None
@@ -24,4 +24,29 @@ class Graph:
             raise IndexError("That vertex does not exist!")
 
 def earliest_ancestor(ancestors, starting_node):
-    pass 
+    # first build the graph
+    graph = Graph() 
+    for pair in ancestors:
+        graph.add_vertex(pair[0])
+        graph.add_vertex(pair[1])
+        graph.add_edge(pair[1], pair[0])
+
+    # do a BFS storing the path
+    q = Queue()
+    q.enqueue([starting_node])
+    max_path_length = 1
+    earliest_ancestor = -1
+
+    while q.size() > 0:
+        path = q.dequeue()
+        v = path[-1]
+
+        if (len(path) >= max_path_length and v < earliest_ancestor) or (len(path) > max_path_length):
+            earliest_ancestor = v
+            max_path_length = len(path)
+        for neighbor in graph.vertices[v]:
+            path_copy = list(path)
+            path_copy.append(neighbor)
+            q.enqueue(path_copy)
+
+    return earliest_ancestor
